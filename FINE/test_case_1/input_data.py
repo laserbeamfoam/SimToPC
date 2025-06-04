e3731568c63978e8283230feaf2dc0282e1dd201
Simon Rodriguez <simonrodriguezgas@gmail.com>
2025-06-04 23:10:28 +0100

    Minor fix

--- FINE/test_case_1/input_data.py
+++
@@ -1,6 +0,0 @@
-RUNNING_ON = "LOCAL" # Options: "LOCAL", "MELUXINA", "SONIC"
-OPENFOAM_VERSION = "2412" # Or "FE40"
-
-# FINE for ~2 million cells.
-# COARSE for ~1Million cells. 
-MESH_DENSITY = "FINE" #  
