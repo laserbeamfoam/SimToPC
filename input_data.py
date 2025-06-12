dfb60d3a932592aae74b0c03f04841f701f5e61c
Simon Rodriguez <simonrodriguezgas@gmail.com>
2025-06-12 23:13:45 +0100

    Added CELL_SIZE to input.data.py

--- input_data.py
+++ input_data.py
@@ -1,6 +1,7 @@
-RUNNING_ON = "SONIC" #"LOCAL" # Options: "LOCAL", "MELUXINA", "SONIC"
+RUNNING_ON = "LOCAL" #"SONIC" #"LOCAL" # Options: "LOCAL", "MELUXINA", "SONIC"
 OPENFOAM_VERSION = "2412" # Or "FE40"
 
 # FINE for ~2 million cells.
 # COARSE for ~1Million cells. 
-MESH_DENSITY = "FINE" # "COARSE"  
+MESH_DENSITY = "COARSE"  #"FINE" # "COARSE"  
+CELL_SIZE = 4e-06
