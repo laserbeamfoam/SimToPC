3622d2b685294671988d0f9ff713356a69319c79
Simon Rodriguez <simonrodriguezgas@gmail.com>
2026-01-20 16:20:58 +0000

    Removed some files

--- simtopc/generate/input_files/meluxina_inp.py
+++
@@ -1,3 +0,0 @@
-hostname =  "meluxina" 
-run_address = "/project/home/p200734/Simon/run/"
-OF_LOCATION = "$HOME/OpenFOAM/OpenFOAM-v2412/etc/bashrc"
--- simtopc/generate/input_files/sonic_inp.py
+++
@@ -1,3 +0,0 @@
-hostname =  "sonic"
-run_address = "/home/people/srodriguez/scratch/run/"
-OF_LOCATION = "$HOME/OpenFOAM/OpenFOAM-v2412/etc/bashrc"
